f2ac188fd00168647c7c87ba9b332798cf0a342a
billybirdsong93 <billybirdsong93@gmail.com>
2026-01-19 08:17:18 -0500

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, CalcyourLife!</h1>
+      <p>Your life decision calculator will appear here.</p>
+    </div>
+  )
+}
